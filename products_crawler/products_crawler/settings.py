4486699073a927ac3ce06d01354ebb414c7e169c
Mozgito <mozgito@gmail.com>
2023-07-20 09:59:42 +0800

    Saving scraped data to json. Savin images. Add logs.

--- products_crawler/products_crawler/settings.py
+++ products_crawler/products_crawler/settings.py
@@ -12,13 +12,14 @@ BOT_NAME = "products_crawler"
 SPIDER_MODULES = ["products_crawler.spiders"]
 NEWSPIDER_MODULE = "products_crawler.spiders"
 
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = "INFO"
+LOG_FILE = "logs/error.log"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "products_crawler (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -26,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- products_crawler/products_crawler/spiders/spider_jd.py
+++ products_crawler/products_crawler/spiders/spider_jd.py
@@ -8,12 +8,22 @@ class SpiderJD(scrapy.Spider):
     name = 'spider_jd'
     urls = ['']
 
-    headers = {}
+    headers = {
+        ":authority": "list.jd.com",
+        ":method": "GET",
+        ":scheme": "https",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language:": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "cache-control": "max-age=0",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    }
 
     def start_requests(self):
-        for url in self.start_urls:
-            for page in range(1, 1):
-                yield Request(url + '&page=' + page, headers=self.headers, callback=self.parse, method='GET',
+        for url in self.urls:
+            for page in range(1, 100):
+                yield Request(url + '&page=' + str(page), headers=self.headers, callback=self.parse, method='GET',
                               dont_filter=True)
 
     def parse(self, response):
@@ -22,12 +32,15 @@ class SpiderJD(scrapy.Spider):
         products = response.selector.xpath(".//div[@id='J_goodsList']/ul[@class='gl-warp clearfix']/"
                                            "li[@class='gl-item']")
         for product in products:
+            img_src = product.xpath("./div/div[@class='p-img']/a/img/@src").get()
+            img_data_lazy = "https:" + product.xpath("./div/div[@class='p-img']/a/img/@data-lazy-img").get()
+
             item = ProductsCrawlerItem()
-            item.prod_id = product.attrib("data-sku")  # xpath("@data-sku").get()
-            item.name = product.xpath(".//div/div[@class='p-name']/a/em/text()").extract_first().replace('\n', '') \
-                .replace('\t', '')
-            item.url = f"https://item.jd.com/{product.attrib('data-sku')}.html"
-            item.price = product.xpath(".//div/div[@class='p-price']/strong/i/text()").extract_first()
-            item.image_urls = "https:" + product.xpath(".//div/div[@class='p-img']/a/img/@src").extract_first()
-            item.site = 'jd.com'
+            item['prod_id'] = product.xpath("@data-sku").get()
+            item['name'] = product.xpath("./div/div[@class='p-name p-name-type-3']/a/em/text()").get() \
+                .replace('\n', '').replace('\t', '')
+            item['url'] = f"https://item.jd.com/{product.xpath('@data-sku').get()}.html"
+            item['price'] = product.xpath("./div/div[@class='p-price']/strong/i/text()").get()
+            item['image_urls'] = [img_src] if img_src is not None else [img_data_lazy]
+            item['site'] = 'jd.com'
             yield item
